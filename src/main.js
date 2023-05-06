372ef23a06492dc42b0f66e0b30ce0f7130eb59b
Mona Lisa <zhangyan0317520@gmail.com>
2023-05-06 11:46:06 +0800

    第二次

--- src/main.js
+++ src/main.js
@@ -2,16 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store'
+import axios from 'axios'
 
-import ElementUI from 'element-ui';  //导入组件
+import ElementUI from 'element-ui'
 import VueRouter from 'vue-router'
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI); //全局引用
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   router,
   store,
+  beforeCreate() {
+    Vue.prototype.axios = axios
+  }
 }).$mount('#app')
